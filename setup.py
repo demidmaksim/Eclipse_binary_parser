ce829a7287648c79c860663a1730b9eb5fd52992
demid.maksim <demid.maksim@gmail.com>
2020-12-17 17:27:21 +0300

    get setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='Eclipse_binary_parser',
     version='1.0',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
