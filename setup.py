dbbf41962cdfb6bd08d25e8dd7fce9cabdba816c
DemidMakS <demid.maksim@gmail.com>
2021-02-19 12:59:25 +0300

    v0.2.1 relase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='Eclipse_Binary_Parser',
-    version='0.1.1',
+    version='0.2.1',
     packages=['Eclipse_Binary_Parser'],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     author_email='demid.maksim@gmail.com',
